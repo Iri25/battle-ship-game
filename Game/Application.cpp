d12a751df376520b5ced476d1a9015a3f14f423b
Iri25 <62218894+Iri25@users.noreply.github.com>
2021-03-30 21:16:01 +0300

    Create Application.cpp
    Veersion 1

---
+++ Game/Application.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include "UserInterface.h"
+using namespace std;
+   
+
+int main() 
+{
+    UserInterface userInterface;
+    userInterface.run();
+    return 0;
+}
