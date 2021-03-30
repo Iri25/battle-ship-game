5b6a2be40cd61cfce9a6431c121c8ddb28f35b59
Iri25 <62218894+Iri25@users.noreply.github.com>
2021-03-30 21:31:10 +0300

    Create Ship.cpp
    Version 1

---
+++ Game/Ship.cpp
@@ -0,0 +1,22 @@
+#include "Ship.h"
+
+/*const int rows = 10;
+const int columns = 10;
+int compuetrArea[rows][columns];     
+int userArea[rows][columns];    
+
+int last_nava = -1;
+int last_nava_id = 0;
+
+int last_my_nava = -1;
+int last_my_nava_id = 0;
+
+struct ships 
+{
+    int initialNumberOfShips;          
+    int totalNumberOfShips;    
+    int coordinates[10][2];  
+};
+
+ships computer[10];
+ships user[10];*/
